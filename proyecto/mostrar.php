f3d2caee837ab295ea85dac62665ca5ad76409d4
ElAgux <agustinsimonte8@gmail.com>
2021-08-10 00:38:18 -0300

    d

--- proyecto/mostrar.php
+++ proyecto/mostrar.php
@@ -5,20 +5,26 @@
 </head>
 <body>
 		<table align="center" border="1" bgcolor="green">
-		<td>name</td>
-		<td>type</td>
-		<td>size</td>
+		<td>ID</td>
+		<td>File</td>
+		<td>Type</td>
+		<td>Size</td>
 	<?php
 		include 'config.php';
-		include 'index.php';
-		$sql = "select * from archivos where id ";
+		$sql = "select * from archivos";
 		$query = mysqli_query($conexion, $sql);
 		while ($registro= mysqli_fetch_array ($query)){?>
+			<?php
+			$file_name = $registro['name'];
+			$file_name_final = trim ($file_name);
+			$ruta = "upload/".$file_name_final; 
+			?>
 			<tr>
-				<td><?php echo "<a href=".$ruta.">".$_FILES['file']['name']."</a>;"?> </td>
-				
+				<td><?php echo $registro['id']; ?></td>
+				<td><?php echo "<a href=".$ruta.">$file_name</a>";?></td>
                 <td><?php echo $registro['type']; ?></td>
                 <td><?php echo $registro['size']; ?></td>
+              
               
               </tr>
 
